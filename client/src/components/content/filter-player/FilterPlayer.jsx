b594d163054ab0ddbf27b399b11c7191b713c0e3
aanggaadc <aanggaadc@gmail.com>
2022-03-29 12:00:11 +0700

    finishing create react FORM for Player Data

---
+++ client/src/components/content/filter-player/FilterPlayer.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
+
+
+function FilterPlayer(props) {
+    return (
+        <div className='mt-3'>
+            <Card>
+                <Card.Header>Search Player</Card.Header>
+                <Card.Body>
+                    <Form>
+                        <div className='row'>
+                            <div className='col-md-6'>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control type="text" placeholder="Search By Username"
+                                        onChange={(e) => {
+                                            props.setFilter({ ...props.filter, username: e.target.value })
+                                        }} />
+                                </Form.Group>
+                            </div>
+                            <div className='col-md-6'>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>email</Form.Label>
+                                    <Form.Control type="text" placeholder="Search By Email"
+                                        onChange={(e) => {
+                                            props.setFilter({ ...props.filter, email: e.target.value })
+                                        }} />
+                                </Form.Group>
+                            </div>
+                        </div>
+
+                        <div className='row'>
+                            <div className='col-md-6'>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Experience</Form.Label>
+                                    <Form.Control type="number" placeholder="Search By Player Experience"
+                                        onChange={(e) => {
+                                            props.setFilter({ ...props.filter, experience: e.target.value })
+                                        }} />
+                                </Form.Group>
+
+                            </div>
+                            <div className='col-md-6'>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Level</Form.Label>
+                                    <Form.Control type="number" placeholder="Search By Player Level"
+                                        onChange={(e) => {
+                                            props.setFilter({ ...props.filter, lvl: e.target.value })
+                                        }} />
+                                </Form.Group>
+                            </div>
+                        </div>
+                        <Button className='float-end' variant="primary" onClick={props.filterSearch}>Search</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default FilterPlayer
